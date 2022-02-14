e1deeb80ce613a93eb25f355c8c9d6b1d33be445
danielglennross <danielglennross@gmail.com>
2022-02-14 13:30:08 +0000

    feat: adds shutdown

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,70 @@
-use std::error;
+// mod shutdown;
+//
+// use std::io;
+// use std::sync::Arc;
+// use tokio::sync::{watch, Mutex};
 
-pub type Handler = fn() -> Result<(), Box<dyn error::Error>>;
+// Runner - spin up workers
+// Worker input handler
+// Each worker receives from work channel, processes handler
+// Each handler writes to metric (observable)
 
-pub fn run(h: Handler) -> Result<(), Box<dyn error::Error>> {
-    h()?;
-    Ok(())
-}
+// Scheduler - spawned process
+// Calculates rate, sends work signal
+
+// Processor - spawned process
+// Runs on interval reporting metrics
+
+// pub type Handler = fn() -> io::Result<()>;
+//
+// pub fn run(h: Handler) -> io::Result<()> {
+//     h()?;
+//     Ok(())
+// }
+//
+// pub struct TestRunner {
+//     concurrency: i32,
+//     handler: Handler,
+// }
+//
+// impl TestRunner {
+//     pub fn new(concurrency: i32, handler: Handler) -> Box<TestRunner> {
+//         Box::new(TestRunner {
+//             concurrency,
+//             handler,
+//         })
+//     }
+//
+//     pub async fn run(&self, shutdown: Arc<Mutex<shutdown::Shutdown>>) -> io::Result<()> {
+//         // pub async fn run<T: Stopper + Send + Sync + 'static + ?Sized>(
+//         //     &self,
+//         //     shutdown: Arc<Mutex<T>>,
+//         // ) -> io::Result<()> {
+//         let (tx, rx) = watch::channel(true);
+//
+//         for i in 0..self.concurrency {
+//             let mut rx = rx.clone();
+//             let h = self.handler;
+//             let x = Arc::clone(&shutdown);
+//
+//             tokio::spawn(async move {
+//                 let mut shutdown = x.lock().await;
+//
+//                 while !shutdown.is_shutdown() {
+//                     tokio::select! {
+//                         _ = rx.changed() => {
+//                             h().expect("failed processing")
+//                         }
+//                         _ = shutdown.recv() => {
+//                             // The shutdown signal has been received.
+//                             println!("exiting worker {}", i);
+//                             return
+//                         }
+//                     }
+//                 }
+//             });
+//         }
+//
+//         Ok(())
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,126 @@
-use gunner::run;
-use std::error;
+mod shutdown;
 
-fn main() {
-    let test = || -> Result<(), Box<dyn error::Error>> {
+use async_trait::async_trait;
+use std::sync::Arc;
+
+use tokio::io;
+use tokio::sync::{broadcast, watch, Mutex};
+
+#[tokio::main]
+async fn main() {
+    let handler = || -> io::Result<()> {
         println!("Hello, world!");
         Ok(())
     };
 
-    let result = run(test);
+    let runner = TestRunner::new(2, handler);
+
+    let (tx, mut rx) = broadcast::channel::<()>(32);
+
+    let shutdown: Arc<Mutex<Shutdown>> = Arc::new(Mutex::new(Shutdown {
+        shutdown: false,
+        notify: rx,
+    }));
+
+    let result = runner.run(shutdown).await;
 
     result.expect("oops something went wrong");
+}
+
+#[async_trait]
+pub trait Stopper {
+    //fn new(notify: broadcast::Receiver<()>) -> Self;
+    fn is_shutdown(&self) -> bool;
+    async fn recv(&mut self);
+}
+
+pub struct Shutdown {
+    /// `true` if the shutdown signal has been received
+    pub shutdown: bool,
+    /// The receive half of the channel used to listen for shutdown.
+    pub notify: broadcast::Receiver<()>,
+}
+
+//#[async_trait]
+impl Shutdown {
+    // fn new(notify: broadcast::Receiver<()>) -> Shutdown {
+    //     Shutdown {
+    //         shutdown: false,
+    //         notify,
+    //     }
+    // }
+
+    /// Returns `true` if the shutdown signal has been received.
+    pub fn is_shutdown(&self) -> bool {
+        self.shutdown
+    }
+
+    /// Receive the shutdown notice, waiting if necessary.
+    pub async fn recv(&mut self) {
+        // If the shutdown signal has already been received, then return
+        // immediately.
+        if self.shutdown {
+            return;
+        }
+
+        // Cannot receive a "lag error" as only one value is ever sent.
+        let _ = self.notify.recv().await;
+
+        // Remember that the signal has been received.
+        self.shutdown = true;
+    }
+}
+
+pub type Handler = fn() -> io::Result<()>;
+
+pub fn run(h: Handler) -> io::Result<()> {
+    h()?;
+    Ok(())
+}
+
+pub struct TestRunner {
+    concurrency: i32,
+    handler: Handler,
+}
+
+impl TestRunner {
+    pub fn new(concurrency: i32, handler: Handler) -> Box<TestRunner> {
+        Box::new(TestRunner {
+            concurrency,
+            handler,
+        })
+    }
+
+    pub async fn run(&self, shutdown: Arc<Mutex<Shutdown>>) -> io::Result<()> {
+        // pub async fn run<T: Stopper + Send + Sync + 'static + ?Sized>(
+        //     &self,
+        //     shutdown: Arc<Mutex<T>>,
+        // ) -> io::Result<()> {
+        let (tx, rx) = watch::channel(true);
+
+        for i in 0..self.concurrency {
+            let mut rx = rx.clone();
+            let h = self.handler;
+            let x = Arc::clone(&shutdown);
+
+            tokio::spawn(async move {
+                let mut shutdown = x.lock().await;
+
+                while !shutdown.is_shutdown() {
+                    tokio::select! {
+                        _ = rx.changed() => {
+                            h().expect("failed processing")
+                        }
+                        _ = shutdown.recv() => {
+                            // The shutdown signal has been received.
+                            println!("exiting worker {}", i);
+                            return
+                        }
+                    }
+                }
+            });
+        }
+
+        Ok(())
+    }
 }
